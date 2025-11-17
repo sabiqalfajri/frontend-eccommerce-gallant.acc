90937827d33090ae5df91cc6b06e14fe9c223e43
Sabiq Alfajri <salnafsabiq@gmail.com>
2025-11-18 03:19:01 +0700

    add useCurrentPath and showFooter

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,21 +1,20 @@
 import { Footer } from "@/components/user/Footer"
 import { NavbarUser } from "@/components/user/NavbarUser"
+import { useCurrentPath } from "@/hooks/universal/useCurrentPath";
 import { Outlet } from "react-router-dom"
 
 export const UserLayout = () => {
+    const currentPath = useCurrentPath();
+    const showFooter = ["/", "/about", "contact"].includes(currentPath);
+
     return (
         <>
             <NavbarUser />
             <main 
-            // className="px-2.5 md:px-16 py-3"
-            // style={{
-            //     "--layout-padding" : "0.625rem",
-            //     "--layout-padding-md": "4rem"
-            // } as React.CSSProperties }
             >
                 <Outlet />
             </main>
-            <Footer />
+            {showFooter && <Footer />}
         </>
     )
 }
