df72871ed75d7ba6d54730be577e005861d1f459
Sabiq Alfajri <salnafsabiq@gmail.com>
2025-12-01 03:23:35 +0700

    button components

--- src/pages/Auth/VerifyEmail.tsx
+++ src/pages/Auth/VerifyEmail.tsx
@@ -15,6 +15,7 @@ import { showError, showInfo } from "@/utils/Toast";
 import { ClipLoader } from "react-spinners";
 import { useCooldownResend } from "@/hooks/universal/useCooldownResend";
 import { useLogout } from "@/hooks/auth/useLogout";
+import { Section } from "@/components/common/Section";
 
 const inputOtpSchema = z.object({
     otp: z.string().length(6, { message: "OTP must be 6 digits" }).regex(/^\d+$/, { message: "OTP must be number" }),
@@ -34,18 +35,18 @@ export const VerifyEmail = () => {
     const otpValue = watch('otp') || "";
     const isOtpComplete = otpValue.length === 6;
 
-    useEffect(() => {
-        const getEmail = sessionStorage.getItem("email");
-        if(!getEmail) {
-            showError('Email cannot find');
-            navigate('/auth/sign-up')
-        }
+    // useEffect(() => {
+    //     const getEmail = sessionStorage.getItem("email");
+    //     if(!getEmail) {
+    //         showError('Email cannot find');
+    //         navigate('/auth/sign-up')
+    //     }
         
-        return () => {
-            sessionStorage.removeItem("email")
-            localStorage.removeItem('resend_email')
-        }
-    }, [])
+    //     return () => {
+    //         sessionStorage.removeItem("email")
+    //         localStorage.removeItem('resend_email')
+    //     }
+    // }, [])
 
     const onSubmit = handleSubmit(async (data) => {
         if(!isLoaded) return;
@@ -83,51 +84,56 @@ export const VerifyEmail = () => {
     }
 
     return (
-        <form action="" onSubmit={onSubmit}>
-            <div className={`flex flex-col items-center justify-center h-screen bg-gray-100 ${isSubmitting ? 'pointer-events-none opacity-50' : ''}`}>
-                <h1 className="text-2xl font-bold mb-1">Verify Your Email</h1>
-                <p className="text-gray-600 mb-6 text-sm">Please check your email for a code verification.</p>
-                <Controller
-                    name="otp"
-                    control={control}
-                    defaultValue=""
-                    render={({ field: { onChange, value } }) => (
-                    <InputOTP 
-                    maxLength={6}
-                    value={value}
-                    onChange={onChange}
-                    disabled={isSubmitting}
-                    >
-                        <InputOTPGroup>
-                            <InputOTPSlot index={0} />
-                            <InputOTPSlot index={1} />
-                            <InputOTPSlot index={2} />
-                        </InputOTPGroup>
-                        <InputOTPSeparator />
-                        <InputOTPGroup>
-                            <InputOTPSlot index={3} />
-                            <InputOTPSlot index={4} />
-                            <InputOTPSlot index={5} />
-                        </InputOTPGroup>
-                    </InputOTP> 
-                    )} 
-                />
-                {errors.otp && <p className="text-red-500 text-xs mt-3">{errors.otp.message}</p>}
-                <div className="flex flex-col items-center justify-center gap-2 mt-[1.5rem]"> 
-                    <Button className="w-[350px]" variant="primary" type="submit" disabled={!isOtpComplete || isSubmitting}>
-                        {isSubmitting ? <ClipLoader size={20} /> : 'Verify'}
-                    </Button>
-                    <p className="mt-2 flex gap-1 text-xs text-gray-400 font-normal">
-                        Did'nt receive the email? 
-                        <button className={`ml-1 ${canResend ? 'text-black hover:underline cursor-pointer font-semibold' : 'text-gray-400 pointer-events-none'} `} onClick={handleResend} disabled={!canResend}>
-                            Resend
-                        </button>
-                        {timeLeft ? (
-                           <p>({timeLeft})</p> 
-                        ) : ''}
-                    </p>
+        <Section wrapperClassName="flex items-center justify-center h-screen">
+            <form action="" onSubmit={onSubmit}>
+                <div className={`flex flex-col items-center justify-center h-full ${isSubmitting ? 'pointer-events-none opacity-50' : ''}`}>
+                    <h1 className="text-2xl font-bold mb-1">Verify Your Email</h1>
+                    <p className="text-gray-600 mb-6 text-sm">Please check your email for a code verification.</p>
+                    <Controller
+                        name="otp"
+                        control={control}
+                        defaultValue=""
+                        render={({ field: { onChange, value } }) => (
+                        <InputOTP 
+                        maxLength={6}
+                        value={value}
+                        onChange={onChange}
+                        disabled={isSubmitting}
+                        >
+                            <InputOTPGroup>
+                                <InputOTPSlot index={0} />
+                                <InputOTPSlot index={1} />
+                                <InputOTPSlot index={2} />
+                            </InputOTPGroup>
+                            <InputOTPSeparator />
+                            <InputOTPGroup>
+                                <InputOTPSlot index={3} />
+                                <InputOTPSlot index={4} />
+                                <InputOTPSlot index={5} />
+                            </InputOTPGroup>
+                        </InputOTP> 
+                        )} 
+                    />
+                    {errors.otp && <p className="text-red-500 text-xs mt-3">{errors.otp.message}</p>}
+                    <div className="flex flex-col items-center justify-center gap-2 mt-[1.5rem] w-full"> 
+                        <Button 
+                        className="w-full md:w-[350px]" variant="primary" 
+                        size="lg"
+                        type="submit" disabled={!isOtpComplete || isSubmitting}>
+                            {isSubmitting ? <ClipLoader size={20} /> : 'Verify'}
+                        </Button>
+                        <p className="mt-2 flex gap-1 text-xs text-gray-400 font-normal">
+                            Did'nt receive the email? 
+                            <button className={`ml-1 ${canResend ? 'text-black hover:underline cursor-pointer font-semibold' : 'text-gray-400 pointer-events-none'} `} onClick={handleResend} disabled={!canResend}>
+                                Resend
+                            </button>
+                            {timeLeft ? (
+                            <p>({timeLeft})</p> 
+                            ) : ''}
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </Section>
     )
 }
